4ea932969d68632eed4e5571b950e6250e802017
Oussama_LD <oussama.labiod@inttic.dz>
2022-10-15 18:33:33 -0700

    make a template

---
+++ js/custom.js
@@ -0,0 +1,6 @@
+var myDiv = document.querySelector('.sea'),
+            myUl = document.querySelector('.ul');
+        myDiv.onclick=function(){
+            myUl.classList.toggle('togle')
+          
+        }
